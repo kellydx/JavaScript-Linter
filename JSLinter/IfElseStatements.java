54df17de90de448cfc255267265a688202af94d2
Kelly  Duong <kelly1234@gmail.com>
2017-04-05 02:36:38 -0400

    check the case when the bracket and if/else statement are not on the same line
    This class takes care of the case when the bracket is in the next line
    of the conditional statement and still work if there are extra empty
    lines in between

--- JSLinter/IfElseStatements.java
+++ JSLinter/IfElseStatements.java
@@ -1,32 +1,62 @@
 import java.io.*;
 import java.util.*;
-class 	IfElseStatements 
-{
-   
-    public IfElseStatements (){
+import java.util.ArrayList;
 
-	}
-	
-    public void match (ArrayList<Integer> ifLocation, ArrayList<Integer> elseLocation, String strLine, int i)
-    {
-           if (strLine.contains("if")&& !strLine.contains("else")&& !strLine.contains("{")){
+public class IfElseStatements{
+    List<String> txt;
+    List<String> clonedTxt = new ArrayList<String>();    
+  
+   public IfElseStatements(List<String> text) {
+        txt = text;        
+    }       
+       
+   public void singleIfElse (List<Integer> ifLocation, List<Integer> elseLocation, List<Integer> bracket_missingIf, List<Integer> bracket_missingElse){         
+       clonedTxt.addAll(txt);
+       clonedTxt.removeAll(Arrays.asList(null,"")); 
+       clonedTxt.add("");  
+       int a =0;
+       int b = 0;
+           
+       for (int i=0; i<txt.size(); i++){
+           if (txt.get(i).contains("if")&& !txt.get(i).contains("else")){
                ifLocation.add(i+1);
-             }
+           }
              
-           if ((strLine.contains("else") || strLine.contains("else if"))&& !strLine.contains("{")){
+           if ((txt.get(i).contains("else") || txt.get(i).contains("else if"))){
                elseLocation.add(i+1);
              }             
            
-    }   
+       }              
+       
+       for (int i=0; i<clonedTxt.size()-1; i++){
+           if (clonedTxt.get(i).contains("if") && !clonedTxt.get(i).contains("else" )){
+               if (!clonedTxt.get(i).contains("{") && !clonedTxt.get(i+1).contains( "{")){
+                  bracket_missingIf.add(ifLocation.get(a));                        
+               }       
+               a++;
+           }
+           
+           if ((clonedTxt.get(i).contains("else") || clonedTxt.get(i).contains("else if"))){  
+               if (!clonedTxt.get(i).contains("{") && !clonedTxt.get(i+1).contains( "{")){
+                   bracket_missingElse.add(elseLocation.get(b));  
+               }  
+               b++;
+           }
+                
+        }
+          
+   }
     
-    public void report (ArrayList<Integer> ifLocation, ArrayList<Integer> elseLocation)
-    {
+   public void report (List<Integer> bracket_missingIf, List<Integer> bracket_missingElse)
+   {
         System.out.println("Below are if/else statement that don't have curly bracket ");
-        System.out.println("If statements on lines:  "+ifLocation);        
-        System.out.println("If statements on lines:  "+elseLocation);
-    }
-    
+        System.out.println("If statements on lines:  "+bracket_missingIf);        
+        System.out.println("Else statements on lines:  "+bracket_missingElse);
+   }
+      
 }
+
+
    
    
    
