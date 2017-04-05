ed54f2d0d261d7483b2590dc8adab689f15c11c4
Kelly  Duong <kelly1234@gmail.com>
2017-04-04 22:29:42 -0400

    new version using java regex and pattern class
    Java regex (pattern and matcher) class offer a faster way to match
    regular expressions and it has the group() method which allow you to get
    access to the part of the regular expression inside the group (String)
    
    src: http://tutorials.jenkov.com/java-regex/matcher.html#group-method

--- JSLinter/UnUsedVariable.java
+++ JSLinter/UnUsedVariable.java
@@ -1,57 +1,48 @@
 import java.io.*;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-class UnUsedVariable implements Detector{
-    
+class UnusedVariable implements Detector{
+    //pattern let/var varName = value;
+    Pattern r = Pattern.compile("(let|var)(.*?)\\=(.*?)\\;");
+    //pattern let/var varName;
+    Pattern r1 = Pattern.compile("(let|var)(.*?)\\;");
+    String varName="";             
     ArrayList<String> txt;
-    public UnUsedVariable(ArrayList<String> text) {
-		txt = text;
-	}  
-	
-	/*
-     * Method match():
-     * - scan the input file and add only the lines that contain variable declaration to an arrayList
-     * Parameters:
-     * ArrayList<String> lines  :  list of the text lines that contain variable declarations
-     * String strLine           : each line of the input file
-     * 
-     */
-    public void match (ArrayList<String> lines)
-    {
-         for (int i = 0; i < txt.size(); i++){        
-         if (txt.get(i).matches("(let|var).*=.*;") || txt.get(i).matches("(let|var).*;")){
-            lines.add(txt.get(i).trim());            
-         }
-    }
-             
-    }     
-  
+    public UnusedVariable(ArrayList<String> text) {
+        txt = text;
+    }       
        
     /*
      * Method declaredVar:
-     * - split each lines that contain variable declaration found in method match
-     * to find the variable names
-     * - push the variable names into an arraylist
+     * - Using java regex and pattern class to find the list of declared 
+     *   var in the text file
      * Parameters:
      * ArrayList<String> declaredVar  :  list of the declared variables
      * ArrayList<String> lines        :  list of the text lines that contain variable declarations
      * 
      */
-    public void declaredVar (ArrayList<String> declaredVar, ArrayList<String> lines){
-        for (int i = 0; i < lines.size(); i++){
-             String [] tokens = (lines.get(i)).split("\\W+");
-             for (int k = 0; k < tokens.length; k++) {
-             System.out.println(tokens[k]);  }  
-             for (int j = 0; j < tokens.length; j++) {
-                 if (tokens[j].equals("var") || tokens[j].equals("let")){
-                     declaredVar.add(tokens[j+1]);
-                    }
-                }
-                
-        }
-    }   
-     
-    /*
+    public void declaredVar (ArrayList<String> declaredVar){        
+           
+        for (int i = 0; i < txt.size(); i++){      
+            //pattern let/var varName = value;
+            Matcher m = r.matcher(txt.get(i));
+            //pattern let/var varName;
+            Matcher m1 = r1.matcher(txt.get(i));
+            
+            if (m.find()){
+                varName = m.group().split("\\=")[0].split("let|var")[1].trim();             
+                declaredVar.add(varName);
+            }
+            else if (m1.find()){
+                varName = m1.group().split(";")[0].split("var|let")[1].trim();
+                declaredVar.add(varName);
+            }
+        }        
+    }
+    
+     /*
      * Method unusedVar:
      * - scan through the input file and find the number of times each variable appears 
      * - push the data into a hashmap called "counts" with key: variable name and value: occurrence of variable
@@ -61,9 +52,8 @@ class UnUsedVariable implements Detector{
      * HashMap <String, Integer> counts     :   hashmap holds the occurence of each variable
      * HashMap <String, Integer> location     :   hashmap holds the location of each variable declaration
      * ArrayList<String> txt                :   every line of text in the input file
-     */
-    
-    public void unusedVar (ArrayList<String> declaredVar, HashMap <String, Integer> counts, HashMap <String, Integer> location ){
+     */   
+     public void unusedVar (ArrayList<String> declaredVar, HashMap <String, Integer> counts, HashMap <String, Integer> location ){
         for (int i = 0; i < declaredVar.size(); i++){  
             for (int j = 0; j < txt.size(); j++){             
                 if (txt.get(j).contains(declaredVar.get(i))){            
@@ -77,8 +67,8 @@ class UnUsedVariable implements Detector{
                 }               
             }           
         }
-    }
-   
+    }    
+    
     
     /*
      * Method report:
@@ -100,6 +90,6 @@ class UnUsedVariable implements Detector{
                 }
         }
     }
-            
+           
 }
 
