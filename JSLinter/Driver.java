770c5adbcfe6dd195cc75877659161e30cf582d6
Kelly  Duong <kelly1234@gmail.com>
2017-04-06 02:29:44 -0400

    basic function call to test the methods

--- JSLinter/Driver.java
+++ JSLinter/Driver.java
@@ -1,38 +1,62 @@
 import java.io.*;
 import java.util.*;
+import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Driver{
-    public static HashMap <String, Integer> counts = new HashMap<String, Integer>(); 
-    public static HashMap <String, Integer> location = new HashMap<String, Integer>(); 
-    public static ArrayList<String> lines= new ArrayList<String>();
-    public static ArrayList<String> declaredVar= new ArrayList<String>();
-    public static ArrayList<String> txt= new ArrayList<String>();
-   public static void main(String [] args){
+public class Driver{    
+    
+    public static List<String> txt= new ArrayList<String>();    
+    
+    public static void main(String [] args)throws IOException {
        
       try{
-        //read the entire text into an arrayList
-        FileInputStream fstream = new FileInputStream("rainbow.txt");
-        DataInputStream in = new DataInputStream(fstream);
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        String strLine;    
-              
-        while ((strLine = br.readLine()) != null)   {
-            txt.add(strLine.trim());                        
-        }       
-        in.close();        
-       }    
-       catch (Exception e){//Catch exception if any
+            //read the entire text into an arrayList
+            FileInputStream fstream = new FileInputStream("rainbow.txt");
+            DataInputStream in = new DataInputStream(fstream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String strLine;    
+            int i=0;
+            while ((strLine = br.readLine()) != null)   {
+                txt.add(strLine.trim());           
+            }       
+            in.close();        
+        }    
+        catch (Exception e){//Catch exception if any
             System.err.println("Error: " + e.getMessage());
-        }      
-        findUnUsedVar(txt);
+      }      
+            findUnusedVar(txt);
+            findSingleIfELse(txt);        
+            findUndeclaredFunction(txt);
+            findBracket();
+        
    }
    
-   public static void findUnUsedVar (ArrayList<String> txt){
-       Detector s = new UnUsedVariable(txt);
-        s.match(lines);        
-        System.out.println(lines);
-        s.declaredVar(declaredVar, lines);        
-        s.unusedVar(declaredVar,counts,location);
-        s.report(counts, location);        
+   public static void findUnusedVar (List<String> txt){
+       UnusedVariable s = new UnusedVariable(txt);         
+       s.declaredVar();        
+       s.unusedVar();
+       s.report();             
     }
+    
+   public static void findSingleIfELse (List<String> txt){
+       IfElseStatements v = new IfElseStatements(txt);         
+       v.singleIfElse(); 
+       v.report();       
+             
+   }
+    
+   public static void findUndeclaredFunction (List<String> txt){
+        UndeclareFunction u = new UndeclareFunction(txt);
+        u.functionList();
+        u.functionCall();
+        u.report();
+   }
+    
+   public static void findBracket (){
+       Bracket b = new Bracket();   
+       b.checkBalancedBrackets(txt);
+             
+   }    
 }
