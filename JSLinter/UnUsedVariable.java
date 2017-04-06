f906a785c6bc19809e967e856b8adf53c9c7dd02
Kelly  Duong <kelly1234@gmail.com>
2017-04-06 01:29:42 -0400

    create object to hold lines number and counts
    Store the line number and the occurence of an unused variable in one
    object, instead of using 2 hash maps like before. Also, using an object
    allows to add additional attributes in the future

--- JSLinter/UnUsedVariable.java
+++ JSLinter/UnUsedVariable.java
@@ -3,28 +3,65 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-class UnusedVariable implements Detector{
+/**
+ * @author Kelly Duong
+ * @date 04/01/2017
+ * This class:
+ *      - Finds declared variables that are not used in the code
+ *      - Reports the name of the variable and the line number the variable is declared on.
+ * 
+ * Methods: 
+ *      - declaredVar () : find the list of declared variables
+ *      - unusedVar ()   : find the list of declared but not used variables
+ *      - report( )      : report the output mentioned above
+ * 
+ */
+
+public class UnusedVariable {
     //pattern let/var varName = value;
     Pattern r = Pattern.compile("(let|var)(.*?)\\=(.*?)\\;");
     //pattern let/var varName;
-    Pattern r1 = Pattern.compile("(let|var)(.*?)\\;");
-    String varName="";             
-    ArrayList<String> txt;
-    public UnusedVariable(ArrayList<String> text) {
-        txt = text;
-    }       
-       
-    /*
-     * Method declaredVar:
-     * - Using java regex and pattern class to find the list of declared 
-     *   var in the text file
-     * Parameters:
-     * ArrayList<String> declaredVar  :  list of the declared variables
-     * ArrayList<String> lines        :  list of the text lines that contain variable declarations
-     * 
+    Pattern r1 = Pattern.compile("(let|var)(.*?)\\;");               
+    List<String> txt = new ArrayList<String>();
+    ArrayList<String> declaredVar= new ArrayList<String>();
+    HashMap <String, UnusedVariable> unused_var_collection = new HashMap<String, UnusedVariable>(); 
+    int line;
+    int count;    
+    public UnusedVariable(List<String> text) {
+        txt = text;        
+        this.line = line;
+        this.count = count;        
+    }    
+    
+	public void setCount(int c){
+		this.count = c;
+	}
+
+	public int getCount(){
+		return this.count;
+    }
+
+	public void setLine(int l){
+		this.line = l;
+	}
+
+	public int getLine(){
+		return this.line;
+	}	
+	
+    public String toString(){
+	    return("  "+this.line);
+	   }   
+	   
+    /**
+     * Method declaredVar():
+     *      - Using java regex matcher and pattern class to find the declared 
+     *        variables in the text file, then add them to an arrayList 
+     *  
+     *  @ ArrayList<String> declaredVar        :   list of the declared variables
      */
-    public void declaredVar (ArrayList<String> declaredVar){        
-           
+    public void declaredVar (){        
+        String varName="";     
         for (int i = 0; i < txt.size(); i++){      
             //pattern let/var varName = value;
             Matcher m = r.matcher(txt.get(i));
@@ -42,51 +79,63 @@ class UnusedVariable implements Detector{
         }        
     }
     
-     /*
+     /**
      * Method unusedVar:
-     * - scan through the input file and find the number of times each variable appears 
-     * - push the data into a hashmap called "counts" with key: variable name and value: occurrence of variable
-     * - Whenever the variable is found the first time, also push the line number into a hashmap called "location", with the key is the variable name
-     * Parameters:
-     * ArrayList<String> declaredVar        :   list of the declared variables
-     * HashMap <String, Integer> counts     :   hashmap holds the occurence of each variable
-     * HashMap <String, Integer> location     :   hashmap holds the location of each variable declaration
-     * ArrayList<String> txt                :   every line of text in the input file
+     *      - Scan through the input file and find the number of times each variable appears 
+     *      
+     *      - Whenever the variable is found the first time, create an object to hold the line number 
+     *        and the count of it. Also push it into a hashmap with the key is the variable name    
+     *        
+     *      - When the same variable appears again, simply update the count of the object 
+     *        corresponded to that var
+     * 
+     * @ ArrayList<String> declaredVar                      :   list of the declared variables
+     * 
+     * @ HashMap <String, Integer> unused_var_collection    :   hashmap holds the variable name as the
+     *                                                          key and the value is an object that holds the 
+     *                                                          line number and occurence of that variable     
+     * 
+     * @ ArrayList<String> txt                              :   every line of text in the input file
      */   
-     public void unusedVar (ArrayList<String> declaredVar, HashMap <String, Integer> counts, HashMap <String, Integer> location ){
-        for (int i = 0; i < declaredVar.size(); i++){  
+     public void unusedVar (){
+       
+         for (int i = 0; i < declaredVar.size(); i++){  
             for (int j = 0; j < txt.size(); j++){             
-                if (txt.get(j).contains(declaredVar.get(i))){            
-                    if (counts.get(declaredVar.get(i)) == null) {
-                        counts.put(declaredVar.get(i),1);
-                        location.put(declaredVar.get(i),j+1);              
+                if (txt.get(j).contains(declaredVar.get(i))){ 
+                    if (unused_var_collection.get(declaredVar.get(i)) == null) {
+                        UnusedVariable v = new UnusedVariable(txt);                    
+                        v.setCount(1);
+                        v.setLine(j+1);  
+                        unused_var_collection.put(declaredVar.get(i),v);
                     } 
-                    else if (counts.get(declaredVar.get(i)) >= 1) {
-                        counts.put(declaredVar.get(i),counts.get(declaredVar.get(i)) + 1);
+                    else if (unused_var_collection.get(declaredVar.get(i)).getCount() >= 1) {
+                        unused_var_collection.get(declaredVar.get(i)).setCount(unused_var_collection.get(declaredVar.get(i)).getCount()+1);
                     }
+                    
                 }               
-            }           
+            }  
+           
         }
     }    
     
-    
-    /*
+    /**
      * Method report:
      * - reports the unused variables based on the occurrence of them
-     *      ~ if a variable only appear one time in the text file, it is an unused variable
-     *      ~ the hashmap "counts" keeps track of the occurences of every variables for future use, if needed
-     * Parameters:     
-     * HashMap <String, Integer> counts     :   hashmap holds the occurence of each variable
-     * HashMap <String, Integer> counts     :   hashmap holds the location of each variable declaration
-     * 
+     *      ~ if a declared variable only appear one time in the text file, it is an unused variable
+     *      ~ the hashmap "unused_var_collection" keeps track of the occurences of every variables for future use, if needed
+     *     
+     * @ HashMap <String, Integer> unused_var_collection     :   hashmap holds the variable name as the key 
+     *                                                           and the value is an object that holds the 
+     *                                                           line number and occurence of that variable   
      */   
     
-    public void report( HashMap <String, Integer> counts, HashMap <String, Integer> location){
+    public void report( ){
         System.out.println("Below are the list of unused variables and the line numbers which they are declared on");
         System.out.println("Variable Name"+ "\t\t" + "Line Number");
-        for (String key : counts.keySet()) {                  
-                if (counts.get(key)==1){                      
-                    System.out.println(key + "\t\t\t  "+location.get(key)); 
+                
+        for (String key : unused_var_collection.keySet()) {                  
+                if ((unused_var_collection.get(key)).getCount()==1){                      
+                    System.out.println(key + "\t\t\t  "+unused_var_collection.get(key)); 
                 }
         }
     }
